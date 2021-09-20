ee2d18ce926bd468fa3a0e9d955a96d35756b4ca
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-09-20 17:24:19 -0400

    About view controller

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Hayley Robinson on 9/20/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
